110109b7df14f514a67007ed53f89a5457bb8480
Layzzi <Shadow_sock@icloud.com>
2021-05-22 16:09:42 +0800

    Revert "something"
    This reverts commit d8b674e897f3c7ac89b3eb13404eb01d2d63ee86.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001/',
+        target: 'http://46.101.154.24:80/',
         ws: true,
         changeOrigin: true
       },
